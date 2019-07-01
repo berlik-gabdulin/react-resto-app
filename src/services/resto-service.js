c86fc7e2761f230237fc676a5d8cea6c2fe0fd4c
Berlik Gabdulin <berlik.gabdulin@gmail.com>
2019-07-01 22:34:29 +0600

    Lesson 14, homework

--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,10 @@
 export default class RestoService {
     getMenuItems() {
-        return [];
+        const menu = async () => {
+            const menuArr = await fetch('http://localhost:3001/menu/')
+                .then(res => res.json());
+            return menuArr;
+        }
+        return menu();
     }
 }
